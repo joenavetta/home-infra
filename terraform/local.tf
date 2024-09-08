91159d93927674e81efcf517a046a6b0ca493543
joenavetta <jnavetta@gmail.com>
2024-09-08 07:55:31 -0400

    Added database server and DHCP

--- terraform/local.tf
+++ terraform/local.tf
@@ -12,17 +12,18 @@ locals {
 
   dns = {
     a_records = {
-      dns         = "10.0.1.1",
-      proxmox-01  = "10.0.1.3",
-      ca          = "10.0.1.6",
-      synology-01 = "10.0.1.4",
-      synology-02 = "10.0.1.5",
-      monitor     = "10.0.1.7",
-      ha          = "10.0.1.8",
-      dhcp        = "10.0.1.9",
-      mail        = "10.0.1.10",
-      ups         = "10.0.1.11",
-      omada       = "10.0.1.12",
+      dns           = "10.0.1.1",
+      proxmox-01    = "10.0.1.3",
+      ca            = "10.0.1.6",
+      synology-01   = "10.0.1.4",
+      synology-02   = "10.0.1.5",
+      monitor       = "10.0.1.7",
+      homeassistant = "10.0.1.8",
+      dhcp          = "10.0.1.9",
+      mail          = "10.0.1.10",
+      ups           = "10.0.1.11",
+      omada         = "10.0.1.12",
+      postgres      = "10.0.1.13",
     }
 
     cname_records = {
@@ -31,7 +32,7 @@ locals {
   }
 
   proxmox = {
-    node = "pve-01"
+    node = "proxmox-01"
   }
 
   vms = [
@@ -54,6 +55,26 @@ locals {
       vmid         = 7842
       storage_pool = "local-data00"
       disk_size    = "80G"
-    }
+    },
+    {
+      name         = "dhcp"
+      clone        = "ubuntu-22.04"
+      cores        = 2
+      memory       = 2048
+      ipconfig0 = format("ip=%s/%d,gw=%s", local.dns.a_records["dhcp"], local.network.cidr_bits, local.network.gateway)
+      vmid         = 2039
+      storage_pool = "local-data00"
+      disk_size    = "80G"
+    },
+    {
+      name         = "postgres"
+      clone        = "ubuntu-22.04"
+      cores        = 4
+      memory       = 4096
+      ipconfig0 = format("ip=%s/%d,gw=%s", local.dns.a_records["postgres"], local.network.cidr_bits, local.network.gateway)
+      vmid         = 7631
+      storage_pool = "local-data01"
+      disk_size    = "100G"
+    },
   ]
 }
